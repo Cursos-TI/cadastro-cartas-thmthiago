526e9f99842438941e6b4807a9e011f26320b998
Thiago <thiagoyutani@gmail.com>
2026-08-18 16:14:15 +0000

    metade do desafio do mestre e melhorias no código, troca do float para double para ter um número mais preciso

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,28 +6,37 @@ int main() {
     char estado1;
     char codigo1[4];
     char cidade1[50];
+
     int populacao1;
     int pontosTuristicos1;
-    float area1;
-    float pib1;
+    
+    double area1;
+    double pib1;
 
     // densidade e pib percapita - parte do desafio aventureiro
-    float densidade1;
-    float pibPerCapita1;
+    double densidade1;
+    double pibPerCapita1;
+
+    // Super poder - Parte do desafio Mestre
+    double superPoder1;
 
     // Dados da Carta 2
     char estado2;
     char codigo2[4];
     char cidade2[50];
+
     int populacao2;
     int pontosTuristicos2;
-    float area2;
-    float pib2;
+    
+    double area2;
+    double pib2;
 
     // densidade e pib percapta - parte do desafio aventureiro
-    float densidade2;
-    float pibPerCapita2;
+    double densidade2;
+    double pibPerCapita2;
 
+    // super poder carta 2 - parte do desafio Mestre
+    double superPoder2;
 
     // "cadastro da carta 1" parte do desafio novato
 
@@ -46,10 +55,10 @@ int main() {
     scanf("%d", &populacao1);
 
     printf("Digite a area em km2: ");
-    scanf("%f", &area1);
+    scanf("%lf", &area1);
 
     printf("Digite o PIB em Bilhões: ");
-    scanf("%f", &pib1);
+    scanf("%lf", &pib1);
 
     printf("Digite o numero de pontos turisticos: ");
     scanf("%d", &pontosTuristicos1);
@@ -78,10 +87,10 @@ int main() {
     scanf("%d", &populacao2);
 
     printf("Digite a area em km2: ");
-    scanf("%f", &area2);
+    scanf("%lf", &area2);
 
     printf("Digite o PIB em Bilhões: ");
-    scanf("%f", &pib2);
+    scanf("%lf", &pib2);
 
     printf("Digite o numero de pontos turisticos: ");
     scanf("%d", &pontosTuristicos2);
